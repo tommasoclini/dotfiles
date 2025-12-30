3e678b456a5d3e9b87c6df7e97ec3afe33688573
Tommaso Clini <tommasoclini@gmail.com>
2025-12-30 16:03:49 +0100

    add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,25 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    fish_vi_key_bindings
+    function fish_greeting
+    end
+    function fish_mode_prompt
+    end
+end
+
+zoxide init fish | source
+
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+if test -d (brew --prefix)"/share/fish/completions"
+    set -p fish_complete_path (brew --prefix)/share/fish/completions
+end
+
+if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+    set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+end
+
+set ZEPHYR_BASE /home/tommaso/zephyrproject/zephyr
+
+alias clear "printf '\033[2J\033[3J\033[1;1H'"
+alias nv "nvim"
