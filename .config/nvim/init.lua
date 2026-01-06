656ab72f322545c73917dc68e77b3ec2c850eaed
Tommaso Clini <tommasoclini@gmail.com>
2026-01-06 10:45:09 +0100

    neovim scroll, ghostty mouse scroll, cursor and maximize

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -50,6 +50,7 @@ vim.o.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.o.scrolloff = 4
 vim.o.sidescrolloff = 4
+vim.o.scroll = 3
 
 -- Show <tab> and trailing spaces
 vim.o.list = true
