cac2ad1104a1209009f25ef68c12120b417237ef
Tommaso Clini <tommasoclini@gmail.com>
2026-01-11 15:24:33 +0100

    reorder neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,7 @@
 local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
 
+-- OPTIONS
+
 vim.o.encoding = "utf-8"
 vim.o.fileencoding = "utf-8"
 
@@ -10,12 +12,10 @@ if is_windows then
     vim.o.shellxquote = ""
 end
 
-vim.o.tabstop = 4      -- how wide a tab looks
-vim.o.shiftwidth = 4   -- indent size
-vim.o.softtabstop = 4  -- <Tab>/<BS> behavior
+vim.o.tabstop = 4 -- how wide a tab looks
+vim.o.shiftwidth = 4 -- indent size
+vim.o.softtabstop = 4 -- <Tab>/<BS> behavior
 vim.o.expandtab = true -- use spaces instead of tabs
-
--- vim.o.winborder = "rounded"
 
 vim.o.signcolumn = "yes"
 
@@ -23,122 +23,42 @@ vim.o.exrc = true
 
 vim.o.wrap = false
 
-vim.diagnostic.config({ virtual_text = true })
-
--- Set <space> as the leader key
--- See `:help mapleader`
--- NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = " "
-
--- [[ Setting options ]] See `:h vim.o`
--- NOTE: You can change these options as you wish!
--- For more options, you can see `:help option-list`
--- To see documentation for an option, you can use `:h 'optionname'`, for example `:h 'number'`
--- (Note the single quotes)
-
--- Print the line number in front of each line
 vim.o.number = true
-
--- Use relative line numbers, so that it is easier to jump with j, k. This will affect the 'number'
--- option above, see `:h number_relativenumber`
 vim.o.relativenumber = true
 
--- Sync clipboard between OS and Neovim. Schedule the setting after `UiEnter` because it can
--- increase startup-time. Remove this option if you want your OS clipboard to remain independent.
--- See `:help 'clipboard'`
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.cursorline = true
+
+vim.o.scrolloff = 4
+vim.o.sidescrolloff = 4
+
+vim.o.list = true
+
+vim.o.confirm = true
+vim.o.termguicolors = true
+
+vim.o.scroll = 3
+
+vim.diagnostic.config({ virtual_text = true })
+
+vim.g.mapleader = " "
+
 vim.api.nvim_create_autocmd("UIEnter", {
     callback = function()
         vim.o.clipboard = "unnamedplus"
     end,
 })
 
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.o.ignorecase = true
-vim.o.smartcase = true
+-- PLUGINS
 
--- Highlight the line where the cursor is on
-vim.o.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.o.scrolloff = 4
-vim.o.sidescrolloff = 4
-vim.o.scroll = 3
-
--- Show <tab> and trailing spaces
-vim.o.list = true
-
--- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s) See `:help 'confirm'`
-vim.o.confirm = true
-
--- [[ Set up keymaps ]] See `:h vim.keymap.set()`, `:h mapping`, `:h keycodes`
-
--- Use <Esc> to exit terminal mode
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
-
--- Map <A-j>, <A-k>, <A-h>, <A-l> to navigate between windows in any modes
-vim.keymap.set({ "t", "i" }, "<A-h>", "<C-\\><C-n><C-w>h")
-vim.keymap.set({ "t", "i" }, "<A-j>", "<C-\\><C-n><C-w>j")
-vim.keymap.set({ "t", "i" }, "<A-k>", "<C-\\><C-n><C-w>k")
-vim.keymap.set({ "t", "i" }, "<A-l>", "<C-\\><C-n><C-w>l")
-vim.keymap.set({ "n" }, "<A-h>", "<C-w>h")
-vim.keymap.set({ "n" }, "<A-j>", "<C-w>j")
-vim.keymap.set({ "n" }, "<A-k>", "<C-w>k")
-vim.keymap.set({ "n" }, "<A-l>", "<C-w>l")
-
-vim.keymap.set("n", "<leader>f", function()
-    vim.lsp.buf.format({ async = true })
-end, { desc = "Format buffer" })
-
-vim.keymap.set("n", "<leader>lg", "<cmd>LazyGit<cr>", { desc = "LazyGit" })
-
-vim.keymap.set("n", "<S-h>", "<cmd>BufferLineCyclePrev<cr>")
-vim.keymap.set("n", "<S-l>", "<cmd>BufferLineCycleNext<cr>")
-
-vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { desc = "LSP: Rename symbol" })
-
-vim.keymap.set("n", "<leader>i", function()
-    local buf = vim.api.nvim_get_current_buf()
-    local enabled = vim.lsp.inlay_hint.is_enabled({ bufnr = buf })
-    vim.lsp.inlay_hint.enable(not enabled, { bufnr = buf })
-end, { desc = "Toggle LSP inlay hints" })
-
-vim.keymap.set("n", "<leader>o", "<cmd>Oil<cr>", { desc = "Oil file explorer" })
-
-vim.keymap.set("n", "<leader>s", ":Pick files<cr>", { desc = "pick files" })
-
--- [[ Basic Autocommands ]].
--- See `:h lua-guide-autocommands`, `:h autocmd`, `:h nvim_create_autocmd()`
-
--- Highlight when yanking (copying) text.
--- Try it with `yap` in normal mode. See `:h vim.hl.on_yank()`
-vim.api.nvim_create_autocmd("TextYankPost", {
-    desc = "Highlight when yanking (copying) text",
-    callback = function()
-        vim.hl.on_yank()
-    end,
-})
-
--- [[ Create user commands ]]
--- See `:h nvim_create_user_command()` and `:h user-commands`
-
--- [[ Add optional packages ]]
--- Nvim comes bundled with a set of packages that are not enabled by
--- default. You can enable any of them by using the `:packadd` command.
-
--- For example, to add the "nohlsearch" package to automatically turn off search highlighting after
--- 'updatetime' and when going to insert mode
 vim.cmd("packadd! nohlsearch")
 
--- [[ Install plugins ]]
--- Nvim functionality can be extended by installing external plugins.
--- One way to do it is with a built-in plugin manager. See `:h vim.pack`.
 vim.pack.add({
     { src = "https://github.com/neovim/nvim-lspconfig" },
-    { src = "https://github.com/olimorris/onedarkpro.nvim.git" },
     { src = "https://github.com/mason-org/mason.nvim" },
     { src = "https://github.com/mason-org/mason-lspconfig.nvim" },
-    { src = "https://github.com/navarasu/onedark.nvim.git" },
     { src = "https://github.com/folke/which-key.nvim.git" },
     { src = "https://github.com/kdheepak/lazygit.nvim.git" },
     { src = "https://github.com/lewis6991/gitsigns.nvim.git" },
@@ -146,36 +66,20 @@ vim.pack.add({
         src = "https://github.com/saghen/blink.cmp.git",
         version = "v1.8.0",
     },
-    {
-        src = "https://github.com/akinsho/bufferline.nvim.git",
-        version = "v4.9.1",
-    },
     { src = "https://github.com/folke/snacks.nvim.git" },
     { src = "https://github.com/nvim-mini/mini.nvim.git" },
     { src = "https://github.com/stevearc/oil.nvim.git" },
 })
 
-if not is_windows then
-    local onedark = require("onedark")
-    onedark.setup({
-        style = "warmer",
-    })
-    onedark.load()
-
-    vim.cmd("colorscheme onedark")
-else
-    require("onedarkpro").setup()
-
-    vim.cmd("colorscheme onedark")
-end
-
 require("oil").setup()
+
+require("mini.tabline").setup()
+
+require("mini.extra").setup()
 
 require("mini.pairs").setup()
 
 require("mini.bufremove").setup()
-vim.keymap.set("n", "<leader>bd", MiniBufremove.delete, { desc = "My delete buffer" })
-vim.keymap.set("n", "<leader>bw", MiniBufremove.wipeout, { desc = "My wipeout buffer" })
 vim.api.nvim_create_autocmd("BufHidden", {
     callback = function(ev)
         local buf = ev.buf
@@ -208,19 +112,9 @@ require("blink.cmp").setup({
     keymap = {
         preset = "default",
         ["<CR>"] = { "accept", "fallback" },
-        ["<C-s>"] = { "show" }
+        ["<C-s>"] = { "show" },
     },
 })
-
-require("bufferline").setup({
-    options = {
-        always_show_bufferline = false,
-        right_mouse_command = function(id)
-            MiniBufremove.delete(id, true)
-        end,
-    },
-})
-vim.o.termguicolors = true
 
 require("mason").setup()
 require("mason-lspconfig").setup({
@@ -242,3 +136,82 @@ if not is_windows then
 
     vim.lsp.config("clangd", get_clangd())
 end
+
+-- COLORSCHEME
+
+--[[
+if not is_windows then
+    vim.pack.add({ "https://github.com/navarasu/onedark.nvim.git" })
+    local onedark = require("onedark")
+    onedark.setup({
+        style = "warmer",
+    })
+    onedark.load()
+
+    vim.cmd("colorscheme onedark")
+else
+--]]
+vim.pack.add({ "https://github.com/olimorris/onedarkpro.nvim.git" })
+require("onedarkpro").setup()
+
+vim.cmd("colorscheme onedark")
+--  end
+
+-- KEYMAPS
+
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+
+vim.keymap.set({ "t", "i" }, "<A-h>", "<C-\\><C-n><C-w>h")
+vim.keymap.set({ "t", "i" }, "<A-j>", "<C-\\><C-n><C-w>j")
+vim.keymap.set({ "t", "i" }, "<A-k>", "<C-\\><C-n><C-w>k")
+vim.keymap.set({ "t", "i" }, "<A-l>", "<C-\\><C-n><C-w>l")
+vim.keymap.set({ "n" }, "<A-h>", "<C-w>h")
+vim.keymap.set({ "n" }, "<A-j>", "<C-w>j")
+vim.keymap.set({ "n" }, "<A-k>", "<C-w>k")
+vim.keymap.set({ "n" }, "<A-l>", "<C-w>l")
+
+-- Lazygit
+vim.keymap.set("n", "<leader>lg", "<cmd>LazyGit<cr>", { desc = "LazyGit" })
+
+-- Oil
+vim.keymap.set("n", "<leader>o", "<cmd>Oil<cr>", { desc = "Oil file explorer" })
+
+-- MiniBufRemove
+vim.keymap.set("n", "<leader>bd", MiniBufremove.delete, { desc = "My delete buffer" })
+vim.keymap.set("n", "<leader>bw", MiniBufremove.wipeout, { desc = "My wipeout buffer" })
+
+-- pickers
+vim.keymap.set("n", "<leader>ff", MiniPick.builtin.files, { desc = "pick files" })
+vim.keymap.set("n", "<leader>fg", MiniPick.builtin.grep_live, { desc = "pick files" })
+
+-- AUTOCOMMANDS
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(ev)
+        -- local client = vim.lsp.get_client_by_id(ev.data.client_id)
+
+        local buf = ev.buf
+
+        vim.keymap.set("n", "<leader>i", function()
+            local enabled = vim.lsp.inlay_hint.is_enabled({ bufnr = buf })
+            vim.lsp.inlay_hint.enable(not enabled, { bufnr = buf })
+        end, { buffer = buf, desc = "Toggle lsp inlay hints" })
+
+        vim.keymap.set("n", "<leader>s", function()
+            MiniExtra.pickers.lsp({ scope = "document_symbol" })
+        end, { buffer = buf, desc = "Pick document symbols" })
+
+        vim.keymap.set("n", "<leader>f", function()
+            vim.lsp.buf.format({ async = true, bufnr = buf })
+        end, { buffer = buf, desc = "Format buffer with lsp" })
+
+        vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { buffer = buf, desc = "Rename symbol with lsp" })
+    end,
+})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking (copying) text",
+    callback = function()
+        vim.hl.on_yank()
+    end,
+})
