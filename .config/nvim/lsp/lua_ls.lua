3fcbeadb42443fbdf12442d98db0f12bafd57c19
Tommaso Clini <tommasoclini@gmail.com>
2026-04-06 00:14:41 +0200

    fix lua ls scanning

--- .config/nvim/lsp/lua_ls.lua
+++ .config/nvim/lsp/lua_ls.lua
@@ -3,7 +3,15 @@ return {
         Lua = {
             workspace = {
                 library = vim.api.nvim_get_runtime_file("", true),
-            }
-        }
-    }
+                checkThirdParty = false,
+            },
+            diagnostics = {
+                globals = { "vim" },
+            },
+        },
+    },
+    single_file_support = true,
+    root_dir = function()
+        return nil
+    end,
 }
